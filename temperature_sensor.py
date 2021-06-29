beecc3134675ceac61c4ae8e7f282f7f63f2597d
Thomas de Wulf <thomas.de.wulf@telenet.be>
2021-06-29 14:40:42 +0200

    Disable ledstrip

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -9,7 +9,7 @@ from RPi import GPIO
 from temp_sensor_manager import TempSensorManager
 from csv_logger import CsvLogger
 from utils import get_temperature_color
-from led_strip import LedStrip
+# from led_strip import LedStrip
 
 def format_reading(device_name, reading_value):
     current_date = datetime.now().isoformat()
@@ -28,25 +28,25 @@ try:
     LOG_FILE = '/home/pi/log.csv'
 
     temp_manager = TempSensorManager()
-    led_strip = LedStrip(10, board.D18)
+    # led_strip = LedStrip(10, board.D18)
     csv_logger = CsvLogger(LOG_FILE)
     display = Display()
 
-    led_strip_thread = Thread(target=led_strip.run)
-    led_strip_thread.start()
+    # led_strip_thread = Thread(target=led_strip.run)
+    # led_strip_thread.start()
 
     devices = temp_manager.get_devices()
 
     while True:
         readings = read_all(devices)
         mean_temp = statistics.mean(x[1] for x in readings)
-        led_strip.current_color = get_temperature_color(mean_temp)
+        # led_strip.current_color = get_temperature_color(mean_temp)
         text = "{:.2f}°C".format(mean_temp)
         display.update_display(text)
         print(readings)
         time.sleep(180)
 except KeyboardInterrupt:
-    led_strip_thread.running = False
-    led_strip_thread.join()
+    # led_strip_thread.running = False
+   # led_strip_thread.join()
     display.clear()
     GPIO.cleanup()
